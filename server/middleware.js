6645c7066f91edf427d4cbed9f6c520099cb9ed5
nycotin <ntincani@hotmail.it>
2024-03-13 18:46:51 +0100

    Added session handling, account-activation/login/logout endpoints, auth middleware

---
+++ server/middleware.js
@@ -0,0 +1,23 @@
+module.exports.isLoggedIn = (req, res, next) => {
+    try {
+        const { user } = req.session;
+        if(!user){
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        };
+    } catch (err) {
+        console.log(err);
+    }
+    next();
+};
+
+module.exports.isTeacher = (req, res, next) => {
+    const { user } = req.session;
+    if(user.role.name !== 'teacher'){
+        return res.status(403).json({
+            message: 'Forbidden'
+        });
+    };
+    next();
+}
--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dbConnect = require('./db/dbConnect.js');
 const User = require('./db/userModel.js');
 const Course = require('./db/courseModel.js');
+const Role = require('./db/roleModel.js');
 const Lesson = require('./db/lessonModel.js');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
+const MongoStore = require('connect-mongodb-session')(session);
+const cookieParser = require('cookie-parser');
+const { isLoggedIn, isTeacher } = require('./middleware.js');
 
 const app = express();
 
@@ -15,50 +18,138 @@ dbConnect();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["POST", "PUT", "GET", "DELETE", "OPTIONS", "HEAD"],
+    credentials: true,
+  }))
+
+const store = new MongoStore({
+  uri: process.env.DB_URL,
+  collection: 'sessions'
+});
+
+store.on('error', function(error){
+    console.log(error);
+});
 
 app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: false,
+  secret: process.env.SECRET,
+  store,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+      sameSite: false,
+      secure: false,
+    //   secure: process.env.NODE_ENV === "production",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true
+    }
 }));
 
 app.get('/', (req, res) => {
-    console.log(req.session);
+    res.send('Response');
 });
 
 // Test client-server connection
-// app.post('/auth/account-activation', async (req, res) => {
-//     const { email, ID, password } = req.body;
-//     const account = await User.findOne({ ID: ID, email: email });
-//     if(!account){
-//         console.log('No accounts found with the given info');
-//         res.send('Not found')
-//     } else {
-//         const hash = await bcrypt.hash(password, 12);
-//         account.hash = hash;
-//         account.status = 'active';
-//         await account.save();
-//         console.log(account);
-//         res.send('User activated!');
-//     }
-// });
-
-
-app.post('/auth/login', async (req, res) => {
-    const { email, password } = req.body;
-    const account = await User.findOne({ email });
-    if(!account){
-        console.log('No accounts found with the given info');
-        res.send('Not found')
-    } else {
-        const login = await bcrypt.compare(password, account.hash);
-        if(!login) {
-            res.send('Wrong email and/or password');
-        } else {
-            res.send('User authenticated!');
+app.post('/api/auth/account-activation', async (req, res) => {
+    try {
+        const { email, sid, password } = req.body;
+        const user = await User.findOne({ sid, email }).populate('role');
+        if(!user) {
+            return res.status(403).json({
+                message: 'Wrong credentials.'
+            });
         }
+
+        const hash = await bcrypt.hash(password, 12);
+        if(user.hash || user.status){
+            return res.status(403).json({
+                message: 'Account was already activated.'
+            });
+        }
+        user.hash = hash;
+        user.status = 'active';
+        await user.save();
+
+        const { _id, role } = user;
+        const userInfo = { _id, sid, role };
+
+        req.session.user = userInfo;
+
+        console.log('Account activated')
+
+        res.json({
+            message: 'Account activated!',
+            accountStatus: 'active',
+            userInfo
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({
+            message: 'Something went wrong.'
+        });
     }
+});
+
+app.post('/api/auth/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }).populate('role');
+        if(!user){
+            return res.status(403).json({
+                message: 'Wrong email or password.'
+            });
+        } else if (!user.hash || !user.status){
+            return res.status(403).json({
+                message: 'Account was not activated.'
+            });
+        }
+
+        const passwordValid = await bcrypt.compare(password.toString(), user.hash);
+        if(passwordValid){
+            const { _id, sid, role } = user;
+            const userInfo = { _id, sid, role };
+            
+            req.session.user = userInfo;
+
+            console.log('User authenticated')
+
+            res.json({
+                message: 'Authentication successful!',
+                loggedIn: true,
+                user: userInfo
+            });
+        } else {
+            return res.status(404).json({
+                message: 'Wrong email or password.',
+                loggedIn: false
+            });
+        };
+    } catch (err){
+        console.log(err);
+        return res.status(400).json({
+            message: 'Something went wrong.'
+        });
+    };
+
+});
+
+app.get('/api/logout', function (req, res) {
+    req.session.destroy(function(err) {
+        if (err){
+            return res.status(400).json({
+                message: 'Something went wrong.'
+            });
+        }
+    })
+    console.log('User logged out!');
+
+    res.json({
+        message: 'User logged out!',
+        loggedOut: true,
+    });
 });
 
 app.get('/courses/:id', async (req, res) => {
