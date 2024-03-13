f34c28e8498f2fdc9e97d53b585c7eae71c1dd4e
Malalai Safi <malalaiafghansafi@gmail.com>
2024-03-13 20:39:01 +0430

    feat(login): add login handlers

--- FE/src/pages/login.js
+++ FE/src/pages/login.js
@@ -1,9 +1,60 @@
+import { useState } from "react";
+
 function Login() {
-    return (
-      <div>
-        <div>Login</div>
-      </div>
-    );
+  const [userType, setUserType] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleUserTypeChange = (type) => {
+    setUserType(type);
+    setUserEmail('');
+    setPassword('');
+    setError('');
+  };
+
+  const handleLogin = () => {
+    if (!userEmail || !password) {
+      setError('Please enter both email and password.');
+      return;
+    }
+
+    if (error === '') {
+      console.log(`Logging in as ${userType} with Email: ${userEmail} and password: ${password}`);
+
+      setTimeout(() => {
+        if (password === 'correctPassword' && userEmail === 'correctEmail') {
+          alert('Login successful!');
+        } else {
+          setError('Incorrect email or password.');
+        }
+      }, 1000);
+    }
+  };
+  return (
+    <div className="login">
+      {/* <a href='./singin.js'>If there need t be sing in Don't Have an Account?</a> */}
+      <br />
+      <label>Log in as a</label>
+      <select value={userType} onChange={(e) => handleUserTypeChange(e.target.value)}>
+        <option value="student">Student</option>
+        <option value="teacher">Teacher</option>
+      </select>
+      <br />
+      <form>
+        <label>Email:</label>
+        <input type="email" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} />
+        <br />
+        <label>Password:</label>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </form>
+      <br />
+      <button onClick={handleLogin}>Login</button>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+    </div>
+
+  );
   }
-  export default Login;
+
   
+export default Login;
