0a32230ad7ecdbbfae8e07ba98826802dbd16cef
nycotin <ntincani@hotmail.it>
2024-03-17 11:48:37 +0100

    Added endpoints for CRUD actions with courses

--- server/server.js
+++ server/server.js
@@ -153,26 +153,81 @@ app.get('/api/logout', function (req, res) {
     });
 });
 
-app.get('/api/dashboard', isLoggedIn, async (req, res) => {
-    if(req.session.user) {
-        const id = req.session.user._id;
-        const user = await User.findOne({ _id: id }).populate('role');
-        return res.send(user);
-    } else {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-})
+// app.get('/api/dashboard', isLoggedIn, async (req, res) => {
+//     if(req.session.user) {
+//         const id = req.session.user._id;
+//         const user = await User.findOne({ _id: id }).populate('role');
+//         return res.send(user);
+//     } else {
+//         return res.status(401).json({ message: 'Unauthorized' });
+//     }
+// })
 
 app.get('/api/courses', isLoggedIn, async (req, res) => {
-    const id = req.session.user._id;
+    const userId = req.session.user._id;
     try {
-        const user = await User.findOne({ _id: id }).populate('role').populate('courses');
+        const user = await User.findOne({ _id: userId }).populate('role').populate('courses');
         const courses = user.courses;
         res.send(courses);
     } catch (err) {
         res.send(err);
     }
 })
+
+// Create new course (teachers only)
+// app.post('/api/courses')
+
+
+// Display specific course details (shared)
+app.get('/api/courses/:id', isLoggedIn, async (req, res) => {
+    try {
+        const courseId = req.body.courseId;
+        const course = await Course.findOne({ _id: courseId }).populate('lessons');
+        res.send(course);
+    } catch (err) {
+        res.send(err.message);
+    }
+})
+
+// Edit specific course properties (teachers only)
+app.put('/api/courses/:id', isLoggedIn, isTeacher, async (req, res) => {
+    try {
+        const courseId = { _id: req.body.courseId };
+        const updatedData = req.body; 
+        delete updatedData.courseId;
+        const updatedCourse = await Course.findOneAndUpdate(courseId, updatedData);
+        res.send('Course updated');
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+
+// Delete course (+ all existing classes in it)
+app.delete('/api/courses/:id', isLoggedIn, isTeacher, async (req, res) => {
+    try {
+        const courseId = req.body.courseId;
+        await Course.findOneAndDelete({ _id: courseId });
+        res.send('Course deleted');
+    } catch (err) {
+        res.send(err);
+    }
+})
+
+// Display all modules of a course (shared)
+// app.get('/courses/:id/modules')
+
+// Create new module in a course (teachers only)
+// app.post('/courses/:id/modules')
+
+// Update module properties in DB (teachers only)
+// app.put('/courses/:id/modules/:moduleId')
+
+// Delete module (teachers only)
+// app.delete('/courses/:id/modules/:moduleId')
+
+
+
 
 // app.get('/api/test/getdata', async (req, res) => {
 //     const url = 'https://jsonplaceholder.typicode.com/comments/'; // Here the Flask API Url
