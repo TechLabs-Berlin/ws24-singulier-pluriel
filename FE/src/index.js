8c382f1bcfe5020b9e045fe1f5752e307ce7248f
Nosedive14 <magdalena.olkiewicz86@gmail.com>
2024-03-17 22:53:14 +0100

    install react-router

--- FE/src/index.js
+++ FE/src/index.js
@@ -10,7 +10,8 @@ import Courses from "./pages/courses";
 import { ChakraProvider } from "@chakra-ui/react";
 // Import BrowserRouter, Routes, and Route from react-router-dom
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
 //Get a reference to the div with ID root
 const el = document.getElementById("root");
 //Tell React to take control of that element
@@ -21,13 +22,15 @@ root.render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/courses" element={<Courses />} />
-          {/* Placeholder to add more Route components here for other paths */}
-        </Routes>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/courses" element={<Courses />} />
+            {/* Placeholder to add more Route components here for other paths */}
+          </Routes>
+        </QueryClientProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
