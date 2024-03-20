d2e0bc2e004d716eca180953df9d7bc5651b49d5
nycotin <ntincani@hotmail.it>
2024-03-20 19:14:02 +0100

    Added image object

--- server/db/courseModel.js
+++ server/db/courseModel.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Lesson = require('./lessonModel.js');
 const User = require('./userModel.js');
+const cloudinary = require('cloudinary');
 
 const courseSchema = new Schema ({
     title: String,
+    image: {
+        url: String,
+        filename: String
+    },
     description: String,
     csid: String,
     lessons: [
