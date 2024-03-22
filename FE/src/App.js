34e9a1c94dc4ee92ca9186bd3906f36c2609101f
Malalai Safi <malalaiafghansafi@gmail.com>
2024-03-22 16:01:53 +0430

    Test for the login changes

--- FE/src/App.js
+++ FE/src/App.js
@@ -1,37 +1,100 @@
-import React from "react";
-import MainCard from "./components/MainCard";
-import SearchBar from "./components/SearchBar";
-import Dashboard from "./pages/dashboard";
-import UserProfile from "./components/UserProfile";
-import { Flex, Text, Box } from "@chakra-ui/react";
+
+// Import the React and ReactDOM libraries
+import { React, Suspense, useContext, useEffect, useState } from 'react';
+// Import BrowserRouter, Routes, and Route from react-router-dom
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  redirect,
+  Routes
+} from "react-router-dom";
+//Import Chakra
+import { ChakraProvider } from "@chakra-ui/react";
+import axios from 'axios';
+import Courses from "./pages/courses";
+import MainCard from "./MainCard";
+import AuthApi from './AuthApi';
+import Login from './pages/login'
+import Dashboard from './pages/dashboard';
+import { Outlet, Navigate } from 'react-router-dom'
+// const Dashboard = React.lazy(() => import("./pages/dashboard"));
+// const Login = React.lazy(() => import("./pages/login"));
+
+
+
+
+function MainApp() {
+  const Auth = useContext(AuthApi);
+  return (
+    <div>
+      <Box>
+        <Text fontStyle="italic" m={4}>
+          Universita Libera di Livorno
+        </Text>
+        <UserProfile />
+        <Flex
+          direction="column"
+          alignItems="center"
+          mt={{ base: "100px", md: "24px" }}
+        >
+          <Flex
+            direction={{ base: "column", md: "row" }}
+            wrap="wrap"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <MainCard title="Courses" />
+            <MainCard title="Communication & Announcements" />
+            <MainCard title="Grade Center" />
+          </Flex>
+          <SearchBar />
+          <Dashboard />
+        </Flex>
+      </Box>
+      <Suspense fallback={<div><h5>Loading....</h5></div>}>
+        <Dashboard />
+        <button onClick={() => Auth.setAuth(false)}>Logout</button>
+
+      </Suspense>
+    </div>
+  );
+}
 
 function App() {
+  const [auth, setAuth] = useState(false);
   return (
-    <Box>
-      <Text fontStyle="italic" m={4}>
-        Universita Libera di Livorno
-      </Text>
-      <UserProfile />
-      <Flex
-        direction="column"
-        alignItems="center"
-        mt={{ base: "100px", md: "24px" }}
-      >
-        <Flex
-          direction={{ base: "column", md: "row" }}
-          wrap="wrap"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <MainCard title="Courses" />
-          <MainCard title="Communication & Announcements" />
-          <MainCard title="Grade Center" />
-        </Flex>
-        <SearchBar />
-        <Dashboard />
-      </Flex>
-    </Box>
-  );
+    <AuthApi.Provider value={{ auth, setAuth }}>
+      <React.StrictMode>
+        <ChakraProvider>
+          <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+              <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/courses" element={<Courses />} />
+                {/* Placeholder to add more Route components here for other paths */}
+              </Routes>
+            </QueryClientProvider>
+          </BrowserRouter>
+        </ChakraProvider>
+      </React.StrictMode>
+    </AuthApi.Provider>
+  )
+}
+const PrivateRoutes = (auth) => {
+  const Auth = useContext(AuthApi);
+  return (
+    Auth.auth ? <Outlet /> : <Navigate to="/login" />
+  )
+}
+const PrivateLogin = (auth) => {
+  const Auth = useContext(AuthApi);
+
+  return (
+    !Auth.auth ? <Outlet /> : <Navigate to="/" />
+  )
 }
 
 export default App;
--- FE/src/index.js
+++ FE/src/index.js
@@ -1,37 +1,22 @@
 // Import the React and ReactDOM libraries
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-//Import App
-import App from "./App";
-import Login from "./pages/login";
-import Dashboard from "./pages/dashboard";
-import Courses from "./pages/courses";
-//Import Chakra
-import { ChakraProvider } from "@chakra-ui/react";
-// Import BrowserRouter, Routes, and Route from react-router-dom
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
+//Import App
+import App, { MainApp } from './App';
+// Import axios
 const queryClient = new QueryClient();
+// Import BrowserRouter, Routes, and Route from react-router-dom
 //Get a reference to the div with ID root
 const el = document.getElementById("root");
 //Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
-
 //Show the component on the screen wrapped with BrowserRouter and Routes
 root.render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/courses" element={<Courses />} />
-            {/* Placeholder to add more Route components here for other paths */}
-          </Routes>
-        </QueryClientProvider>
-      </BrowserRouter>
-    </ChakraProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
 );
+
+
+
