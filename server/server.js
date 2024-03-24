803d311b694744cf55aaa092e6c05441f9cd18c5
nycotin <ntincani@hotmail.it>
2024-03-24 18:29:25 +0100

    Adjusting cookie settings secure: false

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(session({
     rolling: true,
     cookie: {
         sameSite: "lax",
-        secure: true,
+        secure: false,
         maxAge: 1000 * 60 * 60 * 24 * 7,
         httpOnly: false
     }
