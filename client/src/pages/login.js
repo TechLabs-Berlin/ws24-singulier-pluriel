1d978f7cf80eef3ab6cfa44099670fb147e06aca
Malalai Safi <malalaiafghansafi@gmail.com>
2024-04-06 19:58:27 +0430

    new changes of desing

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -66,31 +66,147 @@ function Login() {
     }
     mutate();
   };
-
   return (
-    <Box maxW="sm" borderWidth="1px" borderRadius="lg" p={4} m="40px auto">
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <form onSubmit={handleLogin}>
-        <FormControl id="email" isRequired>
-          <FormLabel>Email address</FormLabel>
-          <Input
-            type="email"
-            value={userEmail}
-            onChange={(e) => setUserEmail(e.target.value)}
-          />
-        </FormControl>
-        <FormControl id="password" isRequired mt={4}>
-          <FormLabel>Password</FormLabel>
-          <Input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </FormControl>
-        <Button mt={4} colorScheme="teal" isLoading={isLoading} type="submit">
-          Login
-        </Button>
-      </form>
+    <Box
+      marginTop={'8%'}
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
+    >
+      <Box
+        display={'flex'}
+        justifyContent={'center'}
+        alignItems={'center'}
+        borderWidth="1px"
+        bgColor={'#01427A;'}
+        boxShadow={'0px 4px 4px 0px #00000040'}
+        width={'452px'}
+        height={'452px'}
+        top={'355px'}
+        left={'587px'}
+        gap={'0px'}
+        borderRadius={'30px'}
+      >
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        <form onSubmit={handleLogin} >
+
+          <FormControl id="email" isRequired>
+            <table>
+              <tr>
+                <td>
+                  <FormLabel
+                    color={'white'}
+                    width={'104px'}
+                    height={'28px'}
+                    top={'205px'}
+                    left={'43px'}
+                    gap={'0px'}
+                    opacity={'0px'}
+                    fontFamily={'Inter'}
+                    fontSize={'20px'}
+                    fontWeight={'500'}
+                    lineHeight={'28px'}
+                    textAlign={'left'}
+                  >
+                    username</FormLabel>
+                </td>
+                <td>
+                  <div className="inputform">
+                    <Input
+                      bgColor={'white'}
+                      width={'244px'}
+                      height={'48px'}
+                      left={'16px'}
+                      padding={'0px 16px 0px 16px'}
+                      gap={'10px'}
+                      borderRadius={'6px'}
+                      borderWidth={'1px'}
+                      borderColor={'#E2E8FO'}
+                      type="email"
+                      value={userEmail}
+                      onChange={(e) => setUserEmail(e.target.value)}
+                    />
+                  </div>
+                </td>
+              </tr>
+            </table>
+          </FormControl>
+          <FormControl id="password" isRequired mt={4}>
+            <table>
+              <tr>
+                <td>
+                  <FormLabel
+                    color={'white'}
+                    width={'104px'}
+                    height={'28px'}
+                    top={'205px'}
+                    left={'43px'}
+                    gap={'0px'}
+                    opacity={'0px'}
+                    fontFamily={'Inter'}
+                    fontSize={'20px'}
+                    fontWeight={'500'}
+                    lineHeight={'28px'}
+                    textAlign={'left'}
+                  >
+                    Password</FormLabel>
+                </td>
+                <td>
+                  <div className="inputform">
+                    <Input
+                      bgColor={'white'}
+                      width={'244px'}
+                      height={'48px'}
+                      left={'16px'}
+                      padding={'0px 16px 0px 16px'}
+                      gap={'10px'}
+                      borderRadius={'6px'}
+                      borderWidth={'1px'}
+                      borderColor={'#E2E8FO'}
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)} />
+
+                  </div>
+                </td>
+              </tr>
+            </table>
+          </FormControl>
+          <table>
+            <tr>
+              <td>
+                <FormLabel
+                  color={'white'}
+                  width={'126px'}
+                  height={'28px'}
+                  marginTop={'20%'}
+                  top={'40px'}
+                  gap={'0px'}
+                  fontFamily={'Inter'}
+                  fontSize={'20px'}
+                  textAlign={'left'}
+                >
+                  Forgot your password?
+                </FormLabel>
+              </td>
+              <td>
+                <Button
+                  width={'101px'}
+                  height={'48px'}
+                  top={'37px'}
+                  left={'70px'}
+                  padding={'0px 24px 0px 24px'}
+                  gap={'8px'}
+                  borderRadius={'6px'}
+                  bgColor={'#E14177'}
+                >
+                  Login
+                </Button>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </Box>
     </Box>
   );
 }
